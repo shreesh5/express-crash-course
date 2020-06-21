9a0eb6555a44739321c5686fbb7f0e0e6aec4f61
Shreesh Nayak <shreeshnayak@gmail.com>
2020-06-21 00:17:04 -0700

    added create member route

--- index.js
+++ index.js
@@ -13,11 +13,15 @@ app.get('/', (req, res) => {
 // Init middleware
 // app.use(logger)
 
+// Body Parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 // Set static folder
 // app.use is used when using middleware
 app.use(express.static(path.join(__dirname, 'public')))
 
-//
+// Members API routes
 app.use('/api/members', require('./routes/api/members'))
 
 const PORT = process.env.PORT || 5000
